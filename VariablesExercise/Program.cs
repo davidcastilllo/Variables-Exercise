20c940919addac38398badda3e9b3f63dc0c8c76
david castillo <seth.castillo4@outlook.com>
2024-11-14 23:25:47 -0600

    Created assigned variables and used string interpolation

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -5,6 +5,16 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            string name = "David";
+            string lastName = "Castillo";
+            int age = 22;
+            char middleInitial = 'S';
+            bool isAwesome = true;
+            double pi = 3.14159;
+            decimal piButDecimal = 3.14159m;
+
+            Console.WriteLine($"Hello my name is {name} {middleInitial} {lastName} and I am {age} years old. It is {isAwesome} I am awesome because I know Pi ({pi}) off the top of my head.");
+
         }
     }
 }
